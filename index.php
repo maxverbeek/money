8588be2eb21a4b0e6c62fb1e7228fc83ab7ca791
Max Verbeek <m4xv3rb33k@gmail.com>
2015-06-02 21:07:33 +0200

    Fck deze shit.. waarom moet hij nou weer niet lui zijn?? tyfus..
    Wel fcking professioneel.. hij gaat er echt spijt van krijgen dat hij zo snel is.. JAMMEREND MARCgit add -A HAD JE MAAR TRAGER NA MOETEN KIJKEN ayyyyyyyy lmao

--- index.php
+++ index.php
@@ -1,83 +1,63 @@
 <?php
 
-$checked = isset($_GET['f']) && in_array($_GET['f'], ['titel', 'band', 'jaar'])
-		? $_GET['f']
-		: 'titel';
+$f = isset($_GET['f']) ? htmlspecialchars($_GET['f']) : 'titel';
 
-$fieldvalue = isset($_GET['search']) ? $_GET['search'] : '';
+$v = isset($_GET['search']) ? htmlspecialchars($_GET['search']) : '';
+
+require 'Database.php';
+
+$db = new Database('platen', 'root', '');
+
+$bands = $db->get('bands')->result();
+
+function kortIn($beschrijving, $limit = 200)
+{
+	return trim(substr($beschrijving, 0, 100)) . '..';
+}
 
 ?>
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Zoek naar een plaat in de database</title>
+	<title>Marc's Database</title>
 	<link rel="stylesheet" href="style.css">
-	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
 </head>
-<body>
-	<h1>Marc's <small style="font-size:0.01em;">kut</small>muziek</h1>
-	<div class="stretch">
-		<div class="query center">
-			<form action="search.php" method="GET">
-				<div class="radio-select">
-					<input type="radio" class="radio-menu" name="f" id="titel" value="titel"<?php echo $checked == 'titel' ? ' checked': ''; ?>><!--
-					--><label class="semi-button" for="titel" data-type="search" data-autocomplete="on">Titel</label><!--
+<body class="intro">
+	<header>
+		<h1>De beste 42 platen van Nederland</h1>
+		<h2>Alleen hier in Marc's Database en op iTunes voor &euro;17,28</h2>
+		<div class="form">
+			<form action="search.php">
 
-					--><input type="radio" class="radio-menu" name="f" id="band" value="band"<?php echo $checked == 'band' ? ' checked' : ''; ?>><!--
-					--><label class="semi-button" for="band" data-type="search" data-autocomplete="on">Band</label><!--
-
-					--><input type="radio" class="radio-menu" name="f" id="jaar" value="jaar"<?php echo $checked == 'jaar' ? ' checked' : ''; ?>><!--
-					--><label class="semi-button" for="jaar" data-type="number" data-autocomplete="off">Jaar</label>
+				<div class="radio-container">
+					<div class="radio">
+						<input type="radio" name="f" id="titel" value="titel" <?php echo $f == 'titel' ? 'checked' : '';?>>
+						<label for="titel">Titel</label>
+						<input type="radio" name="f" id="band" value="band" <?php echo $f == 'band' ? 'checked' : '';?>>
+						<label for="band">Band</label>
+						<input type="radio" name="f" id="jaar" value="jaar" <?php echo $f == 'jaar' ? 'checked' : '';?>>
+						<label for="jaar">Jaar</label>
+					</div>
 				</div>
 
-				<div class="query-input">
-					<input required autofocus type="search" name="search" id="search" placeholder="<?php echo ucfirst($checked); ?>" value="<?php echo $fieldvalue; ?>">
-					<span id="empty" class="cancel">&#x2715;</span>
+				<div class="inputfield">
+					<input type="text" name="search" placeholder="Zoek hier.." autocomplete="off" required value="<?php echo $v ?: ''; ?>">
+					<input type="submit" value="">
 				</div>
-				<input type="submit" value="Zoek">
+
 			</form>
 		</div>
-	</div>
+		<a href="#begin" class="down" title="Lees meer"></a>
+	</header>
+	<section id="begin" class="bands">
+		<h1>Informatie over Bands</h1>
 
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-	<script>
-		$(function ()
-		{
-			labels = $('.semi-button');
-			text = $('#search');
-			empty = $('#empty');
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur perferendis porro placeat excepturi, aut. Magnam odio necessitatibus, expedita sed eos iure maxime, tenetur amet dolorum, deserunt placeat quis exercitationem optio officia ratione ex, rem ea beatae repellat. Minima repellat at corporis magni quas! Magni inventore cum eius quia. Sapiente in, quos perspiciatis nobis dolorem repellat doloribus. Officia beatae tempore rem necessitatibus debitis veniam delectus molestiae excepturi illo, ab placeat eius aspernatur suscipit, aliquam consequuntur ad cumque sunt ducimus! Repellat odit animi ex velit fugiat minima, ipsam atque voluptas expedita praesentium. Soluta sit nulla, a ratione molestiae, quos cum provident, odio reiciendis corporis reprehenderit nesciunt illo fugiat. Totam doloribus iusto atque sunt debitis et consequatur molestiae fugit delectus sed magnam numquam commodi suscipit tempora, ut pariatur placeat eum quis quam aliquam sequi itaque eligendi non. Totam officia voluptates, rerum adipisci ad, quod pariatur beatae error. Corrupti maiores modi sapiente expedita dolor beatae consequuntur, laboriosam? Voluptatem itaque eos quas harum ut animi, repellendus repellat consequuntur assumenda, et facilis vero corporis quam quae? Voluptatum ut maiores maxime quod a rem perspiciatis autem exercitationem, repellendus nesciunt quo, quos optio repudiandae nam nisi! Mollitia culpa, autem eveniet! Quaerat expedita autem, iusto maxime quae doloremque sit quidem, distinctio blanditiis totam unde quis earum veniam, corrupti praesentium laudantium. Dolorem praesentium expedita natus suscipit facere accusamus maxime quasi nulla officiis, provident sunt, ipsam vero nostrum fugit rerum! Repellendus eos, dolorum praesentium voluptate, suscipit illo! Excepturi, numquam nesciunt obcaecati voluptate dicta, consequatur non magnam commodi laborum consequuntur sequi rem iste perspiciatis deserunt sunt. Ea voluptas tempora voluptatem earum vero, odio soluta quasi dolore aliquam velit blanditiis deleniti, ducimus dolorem unde delectus error rem ratione, dignissimos fuga officiis totam? Culpa non quibusdam doloribus accusamus! Commodi esse ullam, quam, nisi aliquid culpa ratione architecto quos. Ex voluptates tempore distinctio at dolor architecto sunt dolores inventore porro. Officiis mollitia nulla maiores quaerat eum, maxime molestias, iste, labore nam voluptatibus, rerum blanditiis. Maiores, suscipit iure hic quidem, eveniet sed neque ullam temporibus nostrum velit aspernatur animi itaque. Esse, quasi earum possimus laborum blanditiis reprehenderit porro ducimus! Sed magni tempore quia, explicabo saepe animi tenetur fugiat facere maxime assumenda. Eligendi facilis magnam, temporibus quos voluptatibus consequatur in! Modi dolorem sunt nihil voluptates temporibus dolor maiores itaque, libero omnis pariatur! Ipsum saepe quam deleniti iure eaque laudantium facere odit quis, quod doloribus in est obcaecati. Iure, nulla. Error sit in sapiente velit accusamus, neque impedit placeat nisi tenetur ab labore quas ratione eveniet asperiores iusto ipsum pariatur corrupti porro expedita dignissimos vero inventore? Similique temporibus laboriosam maiores libero ut explicabo tempore sunt, commodi iure suscipit accusamus nihil iste laborum ab, veritatis officia ratione odit laudantium quidem ex quasi dolore in nam? Molestiae ipsam doloremque dolores omnis nesciunt velit excepturi obcaecati iste est quae optio voluptate quasi, nobis eligendi, temporibus veniam incidunt laborum officiis illo? Tenetur in dignissimos praesentium quasi inventore ipsa accusamus, magni illum ipsam possimus soluta ratione incidunt omnis, eius. Eum tenetur tempore, saepe corrupti consequatur rerum sequi quae similique non, in, alias quibusdam.</p>
+	</section>
 
-			labels.on('click', function ()
-			{
-				// Goede placeholder
-				text.get(0).placeholder = this.innerText;
 
-				// Goede data-type (text|number)
-				text.get(0).type = this.dataset.type;
-
-				// Autocomplete voor number uit
-				text.get(0).autocomplete = this.dataset.autocomplete;
-
-				// Focus het textinvoer element, betere UX
-				text.focus();
-			});
-
-			empty.on('click', function ()
-			{
-				// Maak textbox leeg
-				text.get(0).value = "";
-
-				// Focus het textinvoer element, betere UX
-				text.focus();
-			});
-
-			text.on('focus', function ()
-			{
-				// Cursor aan het einde v/d text on focus
-				text.value = this.value;
-			});
-		});
-	</script>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script src="js/smoothscroll.js"></script>
 </body>
 </html>
